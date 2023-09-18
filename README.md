431094cc113639d894ef5bb97a31e1d88b84859b
Villy Yeoh <yeohvilly@gmail.com>
2023-09-18 18:15:22 -0400

    finished applying the 'past', 'present' and 'future' class to the textarea

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Work-Day-Scheduler
+
+# About
+
+## Description
+
+
+## Screenshot of the website
+
+
+## Link to the website
--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the ht
-console.log(localStorage);
-console.log(localStorage.getItem("hour-9"));
-console.log($(".time-block").length);
-console.log($(".time-block").attr("id"));
+for (x=0, i=9; i>=9 && i<=17; x++, i++) {
+  console.log(typeof parseInt($("#hour-"+[i]).attr("id").split("-")[1]));
+}
+console.log("Current Military Hour:", dayjs().format("HH"));
+
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -15,7 +16,6 @@ $(function () {
   // useful when saving the description in local storage?
   var text;
   for (x=0, i=9; x<$(".time-block").length; x++, i++) {
-    console.log("hi");
     if (localStorage.getItem("hour-"+[i]) === null){
       localStorage.setItem("hour-"+[i], "");
     }
@@ -23,12 +23,11 @@ $(function () {
     $("#hour-"+[i]).children("textarea").text(text);
   }
 
-  $(".saveBtn").on("click", function(event) {
+  $(".saveBtn").on("click", function() {
     for (x=0, i=9; i>=9 && i<=17; x++, i++) {
-      // console.log($(this).parent().attr("id"));
       if($(this).parent().attr("id") === $("#hour-" + [i]).attr("id")) {
         // test
-        console.log($("#hour-" + [i]).attr("id"));
+        console.log($("#hour-"+[i]).attr("id"));
         localStorage.setItem("hour-"+[i], $(this).siblings(".description").val());
         $(this).siblings(".description").val()
       }
@@ -39,7 +38,26 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  
+  console.log(dayjs().format("MMM D, YYYY | dddd | h:mm:ss a | HH:mm"));
+  console.log(dayjs().format("HH"));
+
+  var currentMilitaryHour = parseInt(dayjs().format("HH")) ;
+
+  for (x=0, i=9; i>=9 && i<=17; x++, i++) {
+    var hour = $("#hour-"+[i]).attr("id").split("-")[1];
+    if (hour < currentMilitaryHour) {
+      console.log("<");
+      $("#hour-"+[i]).children("textarea").addClass("past");
+    }
+    if (hour > currentMilitaryHour) {
+      console.log(12, ">", currentMilitaryHour);
+      $("#hour-"+[i]).children("textarea").addClass("future");
+    }
+    if (hour === currentMilitaryHour) {
+      console.log("=");
+      $("#hour-"+[i]).children("textarea").addClass("present");
+    }
+  }
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -47,17 +65,8 @@ $(function () {
   
   // TODO: Add code to display the current date in the header of the page.
   var currentTime = setInterval(function() {
-    $("#currentDay").text(dayjs());
+    $("#currentDay").text(dayjs().format("ddd, MMM D YYYY h:mm:ss a"));
   }, 100);
 });
 
 // parse int (switching to number from string)
-
-// var x = "hour-14";
-// undefined
-// x.split("")
-// (7) ['h', 'o', 'u', 'r', '-', '1', '4']
-// x.split("-")
-// (2) ['hour', '14']
-// x.split("-")[1]
-// '14'
